64166f7a683f93009fda59830aa96d63b99e12bb
Le Huynh <le.huynh@ellisdon.com>
2019-02-18 10:56:42 -0500

    Added handlers

---
+++ accountservice/service/handlers.go
@@ -0,0 +1,21 @@
+/**
+ * Create by Le Trong on 17/Feb/2019
+ */
+
+package service
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/letrong/masonjar-service/accountservice/dao"
+	"github.com/letrong/masonjar-service/accountservice/model"
+)
+
+// CreateAccountHandler creta a person
+func CreateAccountHandler(w http.ResponseWriter, r *http.Request) {
+	var account model.Account
+	_ = json.NewDecoder(r.Body).Decode(&account)
+	dao.InsertOneValue(account)
+	json.NewEncoder(w).Encode(account)
+}
